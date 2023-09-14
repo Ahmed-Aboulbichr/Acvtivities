4e4e34f723663aa39f0e7948d907606b4ed559f7
Ahmed.ABOULBICHR <ABOULBICHRAhmed2@gmail.com>
2023-09-14 09:39:00 +0100

    setup agent for axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuidv4 } from 'uuid';
+import agent from '../api/agent';
 
 function App() {
 
@@ -13,10 +13,14 @@ function App() {
   const [editMode, setEditMode] = useState(false);
 
   useEffect(()=>{
-    axios.get<Activity[]>("http://localhost:5000/api/activities")
-        .then( response => {
-          setActivities(response.data);
-        })
+    agent.Activities.list().then( response => {
+      let activities : Activity[] = [];
+      response.forEach(activity => {
+        activity.date = activity.date.split("T")[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+    })
   }, []);
 
   function handleSelectActivity(id : string){
