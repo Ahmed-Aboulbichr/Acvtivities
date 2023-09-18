700627deb1a3aa2ad0c4c09786a80d449745d99a
Ahmed.ABOULBICHR <ABOULBICHRAhmed2@gmail.com>
2023-09-18 16:41:08 +0100

    finish chapter 6 : add react router

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -28,20 +28,46 @@ export default class ActivityStore {
     }
 
     loadActivities = async ()=> {
+        this.setLoadingInitial(true);
         try{
             const activities = await agent.Activities.list();
-            runInAction( () => {
-                activities.forEach(activity => {
-                    activity.date = activity.date.split("T")[0];
-                    // this.activities.push(activity);
-                    this.activityRegistry.set(activity.id, activity);
-                })
+            activities.forEach(activity => {
+                this.setActivity(activity)
             })
             this.setLoadingInitial(false);
         }catch(error) {
             console.log(error);
             this.setLoadingInitial(false);
         }
+    }
+
+    loadActivity = async (id: string)=> {
+        let activity = this.getActivity(id);
+        if(activity) {
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else{
+            this.setLoadingInitial(true);
+            try{
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(()=> this.selectedActivity = activity);
+                this.setLoadingInitial(false);
+                return activity;
+            }catch(error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+    private setActivity = (activity: Activity)=>{
+        activity.date = activity.date.split("T")[0];
+        // this.activities.push(activity);
+        this.activityRegistry.set(activity.id, activity);
+    }
+    private getActivity = (id: string)=>{
+        return this.activityRegistry.get(id);
     }
 
     setLoadingInitial = (state : boolean) => {
